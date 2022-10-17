8ff797d8fc6e78c2075103e36ab1c5b3e00e01bc
Naresh Thapa <thapanaresh8412@gmail.com>
2022-10-17 23:32:41 +0530

    ok last done

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+
 const http=require('http');
-const server=http.createServer((req,res)=>{
+/*const server=http.createServer((req,res)=>{
     res.write("Hlo World")
 
     console.log('Request From browser To Server');
+    res.end();
+}).listen(4500);*/
+
+
+http.createServer((req,res)=>{
+    res.write("Hllo World");
     res.end();
 }).listen(4500);
