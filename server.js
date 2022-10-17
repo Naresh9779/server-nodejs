f8611da423ceeb58a98fd896f14ad72a4dc53ed9
Naresh Thapa <thapanaresh8412@gmail.com>
2022-10-17 23:42:25 +0530

    update

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ http.createServer((req,res)=>{
 
     console.log('Request From browser To Server');
     res.end();
-});
-http.listen(4500,()=>
+}).listen(4500,(req)=>
     {
-   req.write("hi");
+   
    console.log('Listinning..............');
-req.end();
+
     });
