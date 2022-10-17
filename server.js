35c8867e93b13a732e76c6d0599cf6ff38242f29
Naresh Thapa <thapanaresh8412@gmail.com>
2022-10-18 00:04:53 +0530

    update

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const http=require('http');
 http.createServer((req,res)=>{
-    res.write("<h1><b><marquee>Naresh Thapa World</marquee></b></h1>");
+    res.write("<h1><b><marquee>Naresh Thapa Word</marquee></b></h1>");
 
     console.log('Request From browser To Server');
     console.log(req.url);
     console.log(req.method);
     res.end();
-}).listen(4500,(a)=>
+}).listen(4500,()=>
     {
    
    console.log('Listinning..............');
