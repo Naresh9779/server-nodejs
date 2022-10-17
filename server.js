dd7186bbb776d2e20a2c78392aeb3b39f27c7b77
Naresh Thapa <thapanaresh8412@gmail.com>
2022-10-17 22:37:25 +0530

    done

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const http=require('http')
-const server=http.createserver(req,res)=>{
+const server=http.createserver((req,res)=>{
 
-    
-}
+    console.log('Request From browser To Server')
+});
+server.listen(4000,'local host',()=>{
+
+
+console.Consolelog('server Is Listening');
+
+});
