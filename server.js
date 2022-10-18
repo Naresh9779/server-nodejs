2e4a5cf758ef35403f8a5ed1c1e8687861fcabae
Naresh Thapa <thapanaresh8412@gmail.com>
2022-10-18 14:50:57 +0530

    update

--- server.js
+++ server.js
@@ -1,44 +1,44 @@
 
 const http=require('http');
- const fs=require('fs');
+const fs=require('fs');
 const server=http.createServer((req,res)=>{
-    res.write("<h1><b><marquee>Naresh Thapa Word</marquee></b></h1>");
-    let path='./Nodejs';
-   switch(req.url())
+ 
+   let path;
+  switch(req.url)
+  {
+   case '/':
+       path='/Nodejs/index.html';
+       break;
+
+       default:
+           path='/Nodejs/404.html';
+
+
+
+
+
+  }
+   console.log('Request From browser To Server');
+  // onsole.log(req.url);
+   //console.log(req.method);
+   //res.setHeader('Content-Type','txt/html');
+   fs.readFile(path,(err,FileData)=>
    {
-    case '/':
-        path+='/index.html';
-        break;
-
-        default:
-            path+='/404.html'
-
-
-
-
-
-   }
-    console.log('Request From browser To Server');
-   // onsole.log(req.url);
-    //console.log(req.method);
-    //res.setHeader('Content-Type','txt/html');
-    fs.readFile(path,(err,FileData)=>
-    {
-    if(err)
-    {
-    console.log(err);
-   }
-  else
+   if(err)
    {
-    res.end(FileData);
-   }   
+   console.log(err);
+  }
+ else
+  {
+   res.end(FileData);
+  }   
 
-    });
+   });
 
-    
-}).listen(5000,()=>
-    {
    
-   console.log('Listinning..............');
+}).listen(5000,()=>
+   {
+  
+  console.log('Listinning..............');
 
-    });
+   });
